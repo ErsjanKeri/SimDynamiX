c092df0b3d61463cf456166c60ac0c45794bcafc
Anton Scheitler <anton.scheitler@gmail.com>
2024-10-14 19:20:29 +0200

    species header fixed

--- third_party/Species.cpp
+++ third_party/Species.cpp
@@ -1,5 +1,5 @@
-//
-// Created by Ersi Bësi on 6.10.24.
-//
-
 #include "Species.h"
+#include <string>
+
+Species::Species(const string& species_name, const u_int species_color): name(species_name), color(species_color) {
+}
