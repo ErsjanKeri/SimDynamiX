73164e9c4c3893bc9b79bd630556a08cc6c14f6f
Ersjan Këri <ge87cin@mytum.de>
2025-08-09 15:53:38 +0200

    fixed small issue

--- third_party/Species.cpp
+++ third_party/Species.cpp
@@ -1,5 +1,1 @@
 #include "Species.h"
-#include <string>
-
-Species::Species(const string& species_name, const u_int species_color): name(species_name), color(species_color) {
-}
--- third_party/Species.h
+++ third_party/Species.h
@@ -1,16 +1,18 @@
-
 #ifndef SPECIES_H
 #define SPECIES_H
 #include <string>
+#include <iostream>
 using namespace std;
 
 
 class Species {
     // are there many per block or one
     public:
-        string name;
-        int color;
-        Species(const string& species_name, const u_int species_color);
+        Species(const string& species_name, const u_int species_color)
+                : name(species_name), color(species_color) {
+        }
+        string name = "Species";
+        u_int color = 0xffffffff;
 };
 
 #endif
